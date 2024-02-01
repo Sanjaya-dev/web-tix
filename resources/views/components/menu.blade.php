a26ceede0cbd563aeff09c733212ece973500a46
Fernanda <sanjaya.dev20@gmail.com>
2024-02-01 22:37:43 +0700

    add class active in file menu.blade.php

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,8 +1,8 @@
 <nav class="nav flex-column">
     <!-- I begin to speak only when I am certain what I will say is not better left unsaid - Cato the Younger -->
     @foreach ($list as $row)
-        <a class="nav-link" href="#">
-            {{$row['label']}}
-        </a>
+    <a class="nav-link @if($row['label'] == $active) active @endif" href="#">
+        {{$row['label']}}
+    </a>
     @endforeach
 </nav>
