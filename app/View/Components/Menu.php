437be616336de29ddc8639d30402a2953b68b55d
Fernanda <sanjaya.dev20@gmail.com>
2024-02-01 22:27:01 +0700

    create page dashboard,create component menu and add dashboard.scss

---
+++ app/View/Components/Menu.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class Menu extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public $active;
+
+    public function __construct($active)
+    {
+        $this->active = $active;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|string
+     */
+    public function render()
+    {
+        $data = $this->list(); 
+
+        return view('components.menu',['list' => $data,
+                                        'active' => $this->active]);
+    }
+
+    public function list(){
+        return [
+            [
+                'label' => 'Dashboard'
+            ],
+            [
+                'label' => 'Movies'
+            ],
+            [
+                'label' => 'Theaters'
+            ],
+            [
+                'label' => 'Tickets'
+            ],
+            [
+                'label' => 'User'
+            ]
+        ];
+    }
+}
