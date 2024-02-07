2c7206136c9a3b1e8fa2e7b5100352e8ec59213f
Fernanda <sanjaya.dev20@gmail.com>
2024-02-07 23:17:01 +0700

    add softdeletes in model movie and theater

--- app/Models/Theater.php
+++ app/Models/Theater.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Theater extends Model
 {
-    //
+    use SoftDeletes;
+    
+    protected $fillable = [
+        'theater', 'address', 'status',
+    ];
 }
