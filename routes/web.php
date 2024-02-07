44cb4c56b71a9c7667a05a12ad12b6af83e68f0b
Fernanda <sanjaya.dev20@gmail.com>
2024-02-07 23:18:52 +0700

    add route theater

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,7 @@ Route::middleware('auth')->group(function(){
     // dashboard
     Route::get('/dashboard', 'Dashboard\DashboardController@index')->name('dashboard');
     
-    
-    Route::get('/dashboard/theaters', 'Dashboard\TheaterController@index')->name('dashboard.theaters');
-    
+        
     Route::get('/dashboard/tickets', 'Dashboard\TicketController@index')->name('dashboard.tickets');
     
     // movies
@@ -37,6 +35,14 @@ Route::middleware('auth')->group(function(){
     Route::get('/dashboard/movies/{movie}', 'Dashboard\MovieController@edit')->name('dashboard.movies.edit');
     Route::put('/dashboard/movies/{movie}', 'Dashboard\MovieController@update')->name('dashboard.movies.update');
     Route::delete('/dashboard/movies/{movie}', 'Dashboard\MovieController@destroy')->name('dashboard.movies.delete');
+
+    // theaters
+    Route::get('/dashboard/theaters', 'Dashboard\TheaterController@index')->name('dashboard.theaters');
+    Route::get('/dashboard/theaters/create', 'Dashboard\TheaterController@create')->name('dashboard.theaters.create');
+    Route::post('/dashboard/theaters', 'Dashboard\TheaterController@store')->name('dashboard.theaters.store');
+    Route::get('/dashboard/theaters/{theater}', 'Dashboard\TheaterController@edit')->name('dashboard.theaters.edit');
+    Route::put('/dashboard/theaters/{theater}', 'Dashboard\TheaterController@update')->name('dashboard.theaters.update');
+    Route::delete('/dashboard/theaters/{theater}', 'Dashboard\TheaterController@destroy')->name('dashboard.theaters.delete');
 
     // users
     Route::get('/dashboard/users', 'Dashboard\UserController@index')->name('dashboard.users');
