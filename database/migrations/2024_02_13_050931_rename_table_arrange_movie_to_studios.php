c0990741f048ea9c045f25a0b79f4f7202518004
Fernanda <sanjaya.dev20@gmail.com>
2024-02-13 22:55:48 +0700

    rename foreign keys arrange_movie to studios

--- database/migrations/2024_02_13_050931_rename_table_arrange_movie_to_studios.php
+++ database/migrations/2024_02_13_050931_rename_table_arrange_movie_to_studios.php
@@ -13,13 +13,18 @@ class RenameTableArrangeMovieToStudios extends Migration
      */
     public function up()
     {
+        DB::unprepared('ALTER TABLE `arrange_movie` DROP FOREIGN KEY `arrange_movie_movies_id_foreign`; ALTER TABLE `arrange_movie` ADD CONSTRAINT `studios_movies_id_foreign` FOREIGN KEY (`movies_id`) REFERENCES `movies`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
+        DB::unprepared('ALTER TABLE `arrange_movie` DROP FOREIGN KEY `arrange_movie_theater_id_foreign`; ALTER TABLE `arrange_movie` ADD CONSTRAINT `studios_theater_id_foreign` FOREIGN KEY (`theater_id`) REFERENCES `theaters`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
+        
         Schema::table('arrange_movie',function(Blueprint $table){
             $table->renameIndex('arrange_movie_theater_id_foreign','studios_theater_id_foreign');
             $table->renameIndex('arrange_movie_movies_id_foreign','studios_movies_id_foreign');
 
         });
 
+
         Schema::rename('arrange_movie', 'studios');
+        
     }
 
     /**
@@ -29,12 +34,16 @@ class RenameTableArrangeMovieToStudios extends Migration
      */
     public function down()
     {
+        Schema::rename('studios', 'arrange_movie');
+
         Schema::table('arrange_movie',function(Blueprint $table){
             $table->renameIndex('studios_theater_id_foreign','arrange_movie_theater_id_foreign');
             $table->renameIndex('studios_movies_id_foreign','arrange_movie_movies_id_foreign');
 
         });
 
-        Schema::rename('studios', 'arrange_movie');
+        DB::unprepared('ALTER TABLE `arrange_movie` DROP FOREIGN KEY `studios_movies_id_foreign`; ALTER TABLE `arrange_movie` ADD CONSTRAINT `arrange_movie_movies_id_foreign` FOREIGN KEY (`movies_id`) REFERENCES `movies`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
+        DB::unprepared('ALTER TABLE `arrange_movie` DROP FOREIGN KEY `studios_theater_id_foreign`; ALTER TABLE `arrange_movie` ADD CONSTRAINT `arrange_movie_theater_id_foreign` FOREIGN KEY (`theater_id`) REFERENCES `theaters`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;');
+
     }
 }
